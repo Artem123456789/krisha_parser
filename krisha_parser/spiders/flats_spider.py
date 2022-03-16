8a7c16e9a5e9bd4a7ad84fd57e142aaa44e09dd2
Artyom Zhiruev <neznajkinartem357@gmail.com>
2022-03-16 11:35:16 +0600

    Added multiple pages parsing

--- krisha_parser/spiders/flats_spider.py
+++ krisha_parser/spiders/flats_spider.py
@@ -7,9 +7,14 @@ class FlatsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        print(response)
         for flat in response.css("div.a-card__header"):
             yield {
                 "header": flat.css("a.a-card__title::text").get(),
                 "price": flat.css("div.a-card__price::text").get(),
                 "subtitle": flat.css("div.a-card__subtitle::text").get()
             }
+
+        next_page = response.css("a.paginator__btn--next::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
