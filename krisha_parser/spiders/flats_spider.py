d40e2526dde8c0b973f2b4e7797ad833a5085043
Artyom Zhiruev <neznajkinartem357@gmail.com>
2022-03-16 11:17:13 +0600

    Added parse method

--- krisha_parser/spiders/flats_spider.py
+++ krisha_parser/spiders/flats_spider.py
@@ -5,3 +5,11 @@ class FlatsSpider(scrapy.Spider):
     start_urls = [
         "https://krisha.kz/prodazha/kvartiry/karaganda/"
     ]
+
+    def parse(self, response):
+        for flat in response.css("div.a-card__header"):
+            yield {
+                "header": flat.css("a.a-card__title::text").get(),
+                "price": flat.css("div.a-card__price::text").get(),
+                "subtitle": flat.css("div.a-card__subtitle::text").get()
+            }
